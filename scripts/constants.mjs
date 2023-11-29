7992259dbea0a8bb285e6c3c92f5f2890bac5038
Aleksander Storum <alesto95654@stud.noroff.no>
2023-11-29 12:51:01 +0100

    Added sign in API URL to constant.mjs

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -1,5 +1,7 @@
 const apiBaseURL = "https://api.noroff.dev/api/v1";
 
-const endpointRegisterURL = "/social/auth/register";
+const endpointRegisterURL = "/auction/auth/register";
+const endpointSignInURL = "/auction/auth/login";
 
 export const registerURL = apiBaseURL + endpointRegisterURL;
+export const signInURL = apiBaseURL + endpointSignInURL;
