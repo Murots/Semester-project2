c897712fa7646d6a80ddb9d2007abfbd92e6742c
Aleksander Storum <alesto95654@stud.noroff.no>
2023-12-07 13:38:24 +0100

    Updated contentAccess.js to dynamically create profile link with id when signed in + updated html pages

--- scripts/libraries/contentAccess.mjs
+++ scripts/libraries/contentAccess.mjs
@@ -1,20 +1,45 @@
 function handleContentAccess() {
   if (localStorage.getItem("accessToken")) {
+    const username = localStorage.getItem("username");
+    const navBar = document.getElementById("navbarCollapse");
     const signInButton = document.getElementById("sign-in-button");
-    const navProfile = document.getElementById("nav-profile");
-    const navName = document.getElementById("nav-name");
     const navNewListing = document.getElementById("nav-new");
 
     signInButton.style.display = "none";
-
     navNewListing.classList.remove("d-none");
     navNewListing.classList.add("d-flex");
 
-    navProfile.classList.remove("d-none");
-    navProfile.classList.add("d-flex");
-    navName.innerText = localStorage.getItem("username");
+    const navProfile = document.createElement("div");
+    navProfile.id = "nav-profile";
+    navProfile.className = "d-flex align-items-center my-2 my-md-0";
+    navBar.append(navProfile);
 
-    // profileAvatar.className.add("d-flex");
+    const profileAvatar = document.createElement("img");
+    profileAvatar.src = "../images/Default-profile.png";
+    profileAvatar.className = "rounded-circle";
+    profileAvatar.alt = "Profile avatar";
+    navProfile.append(profileAvatar);
+
+    const navName = document.createElement("a");
+    navName.id = "nav-name";
+    navName.className = "nav-link h4 ps-2 my-2 my-md-0";
+    navName.innerText = username;
+    navName.ariaCurrent = "page";
+    navProfile.append(navName);
+
+    const currentUrl = window.location.href;
+    if (currentUrl.includes("/profile/")) {
+      navName.href = "#";
+    } else if (currentUrl.includes("/auctions/") || currentUrl.includes("/new-listing/")) {
+      navName.href = `../profile/index.html?id=${username}`;
+    } else {
+      navName.href = `profile/index.html?id=${username}`;
+    }
+
+    const creditCount = document.createElement("h4");
+    creditCount.className = "h4 py-1 px-2 ms-5 my-2 my-md-0 bg-primary text-white";
+    creditCount.innerText = "XXXX €";
+    navProfile.append(creditCount);
   }
 }
 
