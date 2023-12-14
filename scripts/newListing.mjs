62545a7806acfb68d91e55d8eeb021848565cd4e
Aleksander Storum <alesto95654@stud.noroff.no>
2023-12-14 03:21:35 +0100

    Just added 'current bid' and bid history to listing details

--- scripts/newListing.mjs
+++ scripts/newListing.mjs
@@ -1,5 +1,5 @@
 import { fetchWithToken } from "./services/doFetch.mjs";
-import { ListingsURL } from "./libraries/constants.mjs";
+import { listingsURL } from "./libraries/constants.mjs";
 import { addImageCard } from "./components/createItemAlbum.mjs";
 
 const newListingForm = document.getElementById("form-new-listing");
